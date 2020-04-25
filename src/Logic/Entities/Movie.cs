078070b33921e28511e11657e6ad92496f966827
necula_lidia <necula_lidia@yahoo.com>
2020-04-25 11:06:11 +0200

    Refactor Movie

--- src/Logic/Entities/Movie.cs
+++ src/Logic/Entities/Movie.cs
@@ -2,42 +2,47 @@
 
 namespace Logic.Entities
 {
-    public class Movie : Entity
+    public abstract class Movie : Entity
     {
         public virtual string Name { get; protected set; }
 
-        public virtual LicensingModel LicensingModel { get; protected set; }
+        protected virtual LicensingModel LicensingModel { get; set; }
 
-        public virtual ExpirationDate GetExpirationDate()
-        {
-            switch (LicensingModel)
-            {
-                case LicensingModel.TwoDays:
-                    return (ExpirationDate)DateTime.UtcNow.AddDays(2);
-
-                case LicensingModel.LifeLong:
-                    return ExpirationDate.Infinite;
-
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
+        public abstract ExpirationDate GetExpirationDate();
 
         public virtual Dollars CalculatePrice(CustomerStatus status)
         {
             decimal modifier = 1 - status.GetDiscount();
 
-            switch (LicensingModel)
-            {
-                case LicensingModel.TwoDays:
-                    return Dollars.Of(4) * modifier;
+            return GetBasePrice() * modifier;
+        }
 
-                case LicensingModel.LifeLong:
-                    return Dollars.Of(8) * modifier;
+        protected abstract Dollars GetBasePrice();
+    }
 
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
+    public class TwoDaysMovie : Movie
+    {
+        protected override Dollars GetBasePrice()
+        {
+            return Dollars.Of(4);
+        }
+
+        public override ExpirationDate GetExpirationDate()
+        {
+            return (ExpirationDate)DateTime.UtcNow.AddDays(2);
+        }
+    }
+
+    public class LifeLongMovie : Movie
+    {
+        protected override Dollars GetBasePrice()
+        {
+            return Dollars.Of(8);
+        }
+
+        public override ExpirationDate GetExpirationDate()
+        {
+            return ExpirationDate.Infinite;
         }
     }
 }
